abbb671f4e2056bbf6bd952ac1fe908acae5e7f1
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-02-05 00:49:31 +0100

    Proper Environment

--- src/EnvQ.py
+++ src/EnvQ.py
@@ -1,16 +1,17 @@
-import numpy as np
 import gym
 import random
-from gym import error, spaces, utils
 from gym.utils import seeding
 
 
 class EnvQ(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, max_state=100, action_size=2, timestep_limit=1073741824):
+    def __init__(self, max_state=100, min_state=0, action_size=2, timestep_limit=1073741824, seed = 42):
+        random.seed(seed)
+
         self.timestep_limit = timestep_limit
         self.max_length = max_state
+        self.min_length = min_state
         self.state = random.randint(0, 99)
 
         self.action_low = 0
@@ -21,31 +22,44 @@ class EnvQ(gym.Env):
 
         self.timestep_count = 0
 
-    def step(self, action):
-        reward = 0
+    def step(self, action: int):
         self.timestep_count += 1
         done = False
+        new_state = self.state
+
         if self.timestep_count == self.timestep_limit:
             done = True
 
         reward = 0 - (((self.state / self.max_length) ** 2) + (self.cost_action[action]))
 
-        return self.state, reward, self.done, {}
+        arrival_rate = 0.5
+        service_rate = self.q_action[action]
+
+        if random.random() < arrival_rate:
+            new_state += 1
+
+        if random.random() < service_rate:
+            new_state -= 1
+
+        new_state = max(min(new_state, self.max_length), self.min_length)
+        self.state = new_state
+        return self.state, reward, done, {}
 
     def reset(self):
         self.state = random.randint(0, 99)
         self.timestep_count = 0
 
     def render(self, mode="human"):
-        pass
+        print(self.state)
 
     def close(self):
         pass
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-
 
 if __name__ == '__main__':
     env = EnvQ()
+    env.render()
+    env.step(env.action_low)
+    env.render()
+    env.step(env.action_high)
+    env.render()
