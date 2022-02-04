acd0b06a7f82ba9681c320c897ff3f6c3c9d580c
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-02-04 20:13:13 +0100

    Update EnvQ.py

--- src/EnvQ.py
+++ src/EnvQ.py
@@ -1,20 +1,40 @@
 import numpy as np
 import gym
+import random
 from gym import error, spaces, utils
 from gym.utils import seeding
 
 
-class AllOnes(gym.Env):
+class EnvQ(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, state_size=4, action_size=2, timestep_limit=1073741824):
-        pass
+    def __init__(self, max_state=100, action_size=2, timestep_limit=1073741824):
+        self.timestep_limit = timestep_limit
+        self.max_length = max_state
+        self.state = random.randint(0, 99)
+
+        self.action_low = 0
+        self.action_high = 1
+
+        self.q_action = {self.action_low: 0.51, self.action_high: 0.6}
+        self.cost_action = {self.action_low: 0, self.action_high: 0.01}
+
+        self.timestep_count = 0
 
     def step(self, action):
-        pass
+        reward = 0
+        self.timestep_count += 1
+        done = False
+        if self.timestep_count == self.timestep_limit:
+            done = True
+
+        reward = 0 - (((self.state / self.max_length) ** 2) + (self.cost_action[action]))
+
+        return self.state, reward, self.done, {}
 
     def reset(self):
-        pass
+        self.state = random.randint(0, 99)
+        self.timestep_count = 0
 
     def render(self, mode="human"):
         pass
@@ -25,3 +45,7 @@ class AllOnes(gym.Env):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
+
+
+if __name__ == '__main__':
+    env = EnvQ()
