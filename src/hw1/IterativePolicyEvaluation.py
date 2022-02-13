91b7d25a66b248bad4ef52d13ff26bba7dc37e05
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-02-13 11:31:51 +0100

    Most parts
    Policy Improvement
    Policy Iteration
    Value Iteration
    Most Plots

--- src/hw1/IterativePolicyEvaluation.py
+++ src/hw1/IterativePolicyEvaluation.py
@@ -2,14 +2,15 @@ from EnvQ import EnvQ
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+DISCOUNT_FACTOR = 0.9
+
 class IterativePolicyEvaluation:
 
-    def __init__(self, env: EnvQ, theta=0.00001):
+    def __init__(self, env: EnvQ):
         self.env = env
-        self.theta = theta
 
-
-    def evaluate(self, policy, gamma):
+    def evaluate(self, policy, gamma=DISCOUNT_FACTOR, theta=1e-8):
         V = np.zeros(self.env.max_length)
         while True:
             delta = 0
@@ -20,10 +21,10 @@ class IterativePolicyEvaluation:
                         Vs += action_prob * prob * (reward + gamma * V[next_state])
                 delta = max(delta, np.abs(V[s] - Vs))
                 V[s] = Vs
-            if delta < self.theta:
+            if delta < theta:
                 break
         return V
 
     def plot_value_function(self, V):
-        plt.bar(range(len(V)),V)
+        plt.bar(range(len(V)), V)
         plt.show()
--- src/hw1/Policies.py
+++ src/hw1/Policies.py
@@ -1,3 +1,5 @@
+import copy
+
 from EnvQ import EnvQ
 from IterativePolicyEvaluation import IterativePolicyEvaluation
 import random
@@ -35,39 +37,101 @@ def get_action(policy, state):
     return action
 
 
-class Policy:
-
-    def __init__(self):
-        self.action_low = 0
-        self.action_high = 1
-
-    def act(self, state):
-        return self.action_low
+def q_from_v(env: EnvQ, V, s: int, gamma=DISCOUNT_FACTOR):
+    q = np.zeros(NUM_ACTION)
+    for a in range(NUM_ACTION):
+        for prob, next_state, reward, done in env.transition[s][a]:
+            q[a] += prob * (reward + gamma * V[next_state])
+    return q
 
 
-if __name__ == '__main__':
+def policy_improvement(env: EnvQ, V, gamma=DISCOUNT_FACTOR):
+    policy = np.zeros([env.max_length, NUM_ACTION]) / NUM_ACTION
+    for s in range(env.max_length):
+        q = q_from_v(env, V, s, gamma=gamma)
+        best_a = np.argwhere(q == np.max(q)).flatten()
+        policy[s] = np.sum([np.eye(NUM_ACTION)[i] for i in best_a], axis=0) / len(best_a)
+    return policy
+
+
+def policy_iteration(env: EnvQ, ipe: IterativePolicyEvaluation, gamma=DISCOUNT_FACTOR, theta=1e-8):
+    policy = np.ones([env.max_length, NUM_ACTION]) / NUM_ACTION
+    # print(policy)
+    while True:
+        V = ipe.evaluate(policy=policy, gamma=gamma, theta=theta)
+        new_policy = policy_improvement(env, V)
+
+        # OPTION 1: stop if the policy is unchanged after an improvement step
+        if (new_policy == policy).all():
+            break;
+
+        # OPTION 2: stop if the value function estimates for successive policies has converged
+        # if np.max(abs(policy_evaluation(env, policy) - policy_evaluation(env, new_policy))) < theta*1e2:
+        #    break;
+
+        policy = copy.copy(new_policy)
+    # print(policy)
+    return policy, V
+
+
+def value_iteration(env: EnvQ, gamma=DISCOUNT_FACTOR, theta=1e-8):
+    V = np.zeros(env.max_length)
+    while True:
+        delta = 0
+        for s in range(env.max_length):
+            v = V[s]
+            V[s] = max(q_from_v(env, V, s, gamma))
+            delta = max(delta, abs(V[s] - v))
+        if delta < theta:
+            break
+    policy = policy_improvement(env, V, gamma)
+    return policy, V
+
+
+def plot_difference(v1, v2, tag=""):
+    zip_object = zip(v1, v2)
+    difference = []
+    for v_1, v_2 in zip_object:
+        difference.append(v_1 - v_2)
+
+    plt.bar(range(len(difference)), difference)
+    plt.title(tag)
+    plt.show()
+
+
+def problem1():
     lp = get_lazy_policy()
     ap = get_aggressive_policy()
 
-    #print(lp, ap)
     env = EnvQ()
     ipe = IterativePolicyEvaluation(env=env)
 
     v_lazy = ipe.evaluate(policy=lp, gamma=DISCOUNT_FACTOR)
     # ipe.plot_value_function(v_lazy)
-    # print(v_lazy)
-
     v_aggressive = ipe.evaluate(policy=ap, gamma=DISCOUNT_FACTOR)
-    #ipe.plot_value_function(v_aggressive)
-    # print(v_aggressive)
+    # ipe.plot_value_function(v_aggressive)
     # plt.plot(v_lazy, label = "lazy")
     # plt.plot(v_aggressive, label = "aggressive")
     # plt.legend()
     # plt.show()
 
-    zip_object = zip(v_lazy, v_aggressive)
-    difference = []
-    for v_l, v_a in zip_object:
-        difference.append(v_l - v_a)
-    ipe.plot_value_function(difference)
+    plot_difference(v1=v_lazy, v2=v_aggressive, tag="Lazy - Aggressive")
 
+    return v_lazy, v_aggressive
+
+
+def problem2(lp_v, ap_v):
+    env = EnvQ()
+    ipe = IterativePolicyEvaluation(env=env)
+    pi_p, pi_v = policy_iteration(env=env, ipe=ipe)
+    vi_p, vi_v = value_iteration(env=env)
+
+    plot_difference(pi_v, vi_v, "Policy Iteration - Value Iteration")
+    plot_difference(pi_v, lp_v, "Optimal - Lazy Policy")
+    plot_difference(pi_v, ap_v, "Optimal - Aggressive Policy")
+
+
+
+if __name__ == '__main__':
+    lp_v, ap_v = problem1()
+    problem2(lp_v=lp_v, ap_v=ap_v)
