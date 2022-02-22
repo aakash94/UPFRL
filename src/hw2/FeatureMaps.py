5d306418dacac0cdc21283a0a3b4cce4305ae140
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-02-22 20:27:27 +0100

    Proper Coarse Map

--- src/hw2/FeatureMaps.py
+++ src/hw2/FeatureMaps.py
@@ -14,23 +14,22 @@ class FeatureMaps():
         self.len_q = env.max_length
 
     def get_fine_fm(self):
-        side = self.len_q+1
+        # side = self.len_q
+        side = self.len_q + 1
         shape = (side, side)
         fm = np.zeros(shape)
         np.fill_diagonal(fm, 1)
         return fm
 
     def get_coarse_fm(self):
-        side = int(self.len_q/5)
+        side = int(self.len_q / 5)
         shape = (self.len_q, side)
         fm = np.zeros(shape)
 
-        for x in range(self.len_q+1):
+        for x in range(self.len_q):
             for s in range(side):
-                lower_bound = 5*(s-1)
-                upper_bound = (5*s)-1
-                if x>= lower_bound and x <= upper_bound:
-                    fm[x][s] = 1
+                val = int(x/5)
+                fm[x][val] = 1
         return fm
 
     def get_pwl_fm(self):
@@ -45,3 +44,4 @@ if __name__ == '__main__':
     print("\nFine\n", ffm)
     print("\nCoarse\n", cfm)
     print("\npiecewise linear\n", pwlfm)
+    print("\nDone\n")
