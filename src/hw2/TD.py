26e3e4bd9077ed06b424a746a94b7b04885cb7bb
Miguel <miguel.silva@unmsm.edu.pe>
2022-02-23 15:48:47 +0100

    td kind of working

--- src/hw2/TD.py
+++ src/hw2/TD.py
@@ -7,8 +7,10 @@ from matplotlib import pyplot as plt
 from collections import defaultdict
 
 from EnvQ import EnvQ
-from Policies import policy_iteration, value_iteration
+from Policies import get_lazy_policy, get_aggressive_policy
 from IterativePolicyEvaluation import IterativePolicyEvaluation
+
+SEED = 666
 
 def alpha_function(timestep = 0, a = 10^5, b = 10^5):
     return a/(timestep +b)
@@ -20,17 +22,21 @@ class TD():
     def evaluate (self, policy, num_episodes, alpha_function = alpha_function , gamma = .9):
         V = defaultdict(float)
         
-        for i_episode in range(1, num_episodes+1):
-            state=self.env.reset()
-            while True:
-                action = np.choice(self.env.actions, 1, p=policy[state])
-                next_state, reward, done, _ = self.env.step(action)
-                alpha = alpha_function(i_episode)
-                V[state] += alpha * (reward + gamma * V[next_state] - V[state])
-                if done:
-                    break
+        state=self.env.reset()
+        policy_fun = policy()
+        
+        for i_episode in range(num_episodes):
+            action = np.random.choice(self.env.actions, 1, p=policy_fun[state], seed=SEED)
+            next_state, reward, done, _ = self.env.step(action[0])
+            alpha = alpha_function(i_episode)
+            V[state] += alpha * (reward + gamma * V[next_state] - V[state])
+            state = next_state
+
         return V
 
 
 if __name__ == '__main__':
-    td = TD()
+    env = EnvQ()
+    td = TD(env)
+    V = td.evaluate(get_aggressive_policy, 10^7, alpha_function)
+    print(V)
