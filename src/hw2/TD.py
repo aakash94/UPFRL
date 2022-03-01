4841edd1416386ba844d775e76afa57870467e30
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-03-01 19:21:49 +0100

    Feature Maps

--- src/hw2/TD.py
+++ src/hw2/TD.py
@@ -7,6 +7,7 @@ from EnvQ import EnvQ
 from Policies import get_lazy_policy, get_aggressive_policy, policy_improvement, DISCOUNT_FACTOR, plot_policy
 from IterativePolicyEvaluation import IterativePolicyEvaluation
 from Utils import plot_dict
+from FeatureMaps import FeatureMaps
 
 SEED = 4
 
@@ -22,29 +23,46 @@ class TD():
     def __init__(self, env: EnvQ):
         self.env = env
 
-    def evaluate(self, policy, alpha_function=alpha_function, gamma=DISCOUNT_FACTOR):
+    def get_v(self, theta: np.ndarray, feature_map: np.ndarray):
         V = defaultdict(float)
+        for state in range(self.env.max_length):
+            V[state] = float(np.matmul(theta.T, feature_map[state]))
+        return V
 
+    def evaluate(self, policy, feature_map, alpha_function=alpha_function, gamma=DISCOUNT_FACTOR):
         state = self.env.reset()
+        theta = np.zeros_like(feature_map[state])
         policy_fun = policy()
         done = False
         timestep = 0
         pbar = tqdm(desc="Timesteps Elapsed", total=timestep + 1)
+
         while not done:
             action = np.random.choice(self.env.actions, 1, p=policy_fun[state])
             next_state, reward, done, _ = self.env.step(action[0])
+
+            v_state = np.matmul(theta.T, feature_map[state])
+            v_next_state = np.matmul(theta.T, feature_map[next_state])
+
             alpha = alpha_function(timestep=timestep)
-            V[state] += alpha * (reward + (gamma * V[next_state]) - V[state])
+            delta = (reward + (gamma * v_next_state) - v_state)
+
+            theta += alpha * delta * feature_map[state]
             state = next_state
+
             timestep += 1
             pbar.update(1)
         pbar.close()
-        return V
+        return self.get_v(theta=theta, feature_map=feature_map)
 
 
 if __name__ == '__main__':
-    env = EnvQ(timestep_limit=10e+5, seed=SEED)
+    env = EnvQ(timestep_limit=10e+4, seed=SEED)
     td = TD(env)
-    V = td.evaluate(policy=get_aggressive_policy, alpha_function=alpha_function)
+    fm = FeatureMaps()
+    fine_map = fm.get_fine_fm()
+    coarse_map = fm.get_coarse_fm()
+    pwl_map = fm.get_pwl_fm()
+    V = td.evaluate(policy=get_aggressive_policy, feature_map=fine_map, alpha_function=alpha_function)
     print(V)
     plot_dict(a=V, tag="Approximate Value Function")
